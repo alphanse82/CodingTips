33d18983df498b6644d7433bfb72194fe948728c
alphanse82 <RakeshKumarGupta@outlook.com>
2020-07-28 11:26:32 -0700

    Create BinarySearch.cs

---
+++ BinarySearch.cs
@@ -0,0 +1,19 @@
+// Iterative
+public bool binary_search(int num, int[] input) 
+{
+  int left = 0;
+  int right = input.Length -1;
+    
+    while( left <= right) {
+        long  mid = left + ( right - left ) / 2;
+        
+        if(num == input[mid])
+            return true;
+        
+        if( input[mid] > num)
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+    return false;   
+}
