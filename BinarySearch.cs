cca8605159fc8c135cec81079f61fbbce7d29412
alphanse82 <RakeshKumarGupta@outlook.com>
2021-08-03 22:04:34 -0700

    Update BinarySearch.cs

--- BinarySearch.cs
+++ BinarySearch.cs
@@ -1,20 +1,20 @@
 // Iterative
-public bool binary_search(int num, int[] input) 
-{
+public bool binary_search(int num, int[] input) {
   
   int left = 0;
   int right = input.Length -1;
     
-    while( left <= right) {
-        long  mid = left + ( right - left ) / 2;
+  while( left <= right) {
+    int  mid = left + ( right - left ) / 2;
         
-        if(num == input[mid])
-            return true;
+    if(num == input[mid])
+      return true;
         
-        if( input[mid] > num)
-            right = mid - 1;
-        else
-            left = mid + 1;
-    }
-    return false;   
+    if( input[mid] > num)
+      right = mid - 1;
+    else
+      left = mid + 1;
+  }
+  
+  return false;   
 }
